e6c607142a1cb281afb1d6bb0a7b49934fc00b93
Joseane-Brilhante <joseane.brilhante@academico.ifpb.edu.br>
2023-11-26 22:24:01 -0300

    api comentarios

--- apiComments/app.js
+++ apiComments/app.js
@@ -1,26 +1,50 @@
 const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const redis = require('redis');
+const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3500;
+const client = redis.createClient();
 
-app.use(express.json());
-app.use(cors());
+app.use(bodyParser.json());
 
-// Rota para obter os livros
-app.get('/search/:book', async (req, res) => {
-    const { book } = req.params; 
-  try {
-    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${book}`);
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao acessar a API externa' });
-  }
+// Endpoint para adicionar um comentário
+app.post('/comentarios/:livroId', (req, res) => {
+  const { livroId } = req.params;
+  const { autor, comentario } = req.body;
+
+  const dataHoraPostagem = new Date().toLocaleString();
+
+  const novoComentario = {
+    autor,
+    comentario,
+    dataHoraPostagem,
+  };
+
+  client.hset(`livro:${livroId}:comentarios`, dataHoraPostagem, JSON.stringify(novoComentario), (err, reply) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Erro ao adicionar comentário');
+    } else {
+      res.status(201).send('Comentário adicionado com sucesso');
+    }
+  });
 });
 
+// Endpoint para obter todos os comentários de um livro
+app.get('/comentarios/:livroId', (req, res) => {
+  const { livroId } = req.params;
 
-// Inicializa o servidor na porta especificada
+  client.hgetall(`livro:${livroId}:comentarios`, (err, comentarios) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Erro ao obter comentários');
+    } else {
+      res.status(200).json(comentarios || {});
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`API Gateway rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });

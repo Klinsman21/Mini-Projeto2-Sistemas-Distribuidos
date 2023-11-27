2dbad517ceb56f630894f94378ee07e068fb21c3
klinsman <joseklinsman259@gmail.com>
2023-11-27 00:12:14 -0300

    up

--- apiComments/app.js
+++ apiComments/app.js
@@ -7,22 +7,21 @@ const app = express();
 app.use(cors());
 
 const client = redis.createClient({
-  host: 'localhost', // Substitua pelo seu host do Redis
-  port: 6379, // Substitua pela porta do seu servidor do Redis
+  host: 'localhost',
+  port: 6379, 
 });
 
 
 app.use(bodyParser.json());
 
-// Endpoint para adicionar um comentário
 app.post('/comments/:bookID', (req, res) => {
   const { bookID } = req.params;
-  const { autor, comment, postDate } = req.body; // Adicionando postDate ao JSON enviado pelo cliente
+  const { autor, comment, postDate } = req.body;
   
   const newComment = {
     autor,
     comment,
-    postDate: postDate || new Date().toISOString().split('T')[0], // Usando postDate enviado ou gerando um novo
+    postDate: postDate || new Date().toISOString().split('T')[0],
   };
 
   console.log("ok");
@@ -37,32 +36,14 @@ app.post('/comments/:bookID', (req, res) => {
   });
 });
 
-
-// Defina a porta em que o servidor irá escutar
-
-// Endpoint para obter todos os comentários de um livro
 app.get('/comments/:livroId', (req, res) => {
   const { livroId } = req.params;
 
-  // client.get(livroId, (err, comentarios) => {
-  //   if (err) {
-  //     console.error(err);
-  //     res.status(500).send('Erro ao obter comentários');
-  //   } else {
-  //     let data = JSON.parse(comentarios)
-  //     res.status(200).json(data || {});
-  //   }
-  // });
   client.lrange(livroId, 0, -1, (err, reply) => {
     if (err) {
       console.error(err);
     } else {
-      console.log('Elementos da lista:');
-      // console.log(reply); // `reply` conterá os elementos da lista
       res.status(200).json(reply);
-      reply.forEach(element => {
-        console.log(JSON.parse(element).autor);
-      });
     }
   });
 });
--- apiGateway/index.js
+++ apiGateway/index.js
@@ -8,7 +8,6 @@ const PORT = 3500;
 app.use(express.json());
 app.use(cors());
 
-// Rota para obter os livros
 app.get('/search/:book', async (req, res) => {
   const { book } = req.params;
   try {
@@ -52,8 +51,7 @@ app.get('/comments/:book', async (req, res) => {
     res.status(500).json({ error: 'Erro ao acessar a API externa' });
   }
 });
-
-// Inicializa o servidor na porta especificada
+a
 app.listen(PORT, () => {
   console.log(`API Gateway rodando na porta ${PORT}`);
 });
